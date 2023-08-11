006ad1bb69e0a033f537b3990b815d0bb07dc048
Rosa <rosa.maggi@epfl.ch>
2023-08-11 09:34:57 +0200

    [Fix] Language

--- diploma_verification_form.php
+++ diploma_verification_form.php
@@ -1,19 +1,19 @@
 <div>
     <form method="post" id="verification-form" action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>">
         <div class="form-group">
-            <label for="prenom">Graduate’s First Name</label><br>
+            <label for="prenom"><?php echo $labels['graduateFirstName']; ?></label><br>
             <input  id="prenom" class="form-control" name="prenom" type="text" placeholder="First name..." value="<?php echo isset($_POST["prenom"]) ? esc_attr($_POST["prenom"]) : ''; ?>">
         </div>
         <div class="form-group">
-            <label for="nom">Graduate’s Surname</label><br>
+            <label for="nom"><?php echo $labels['graduateSurname']; ?></label><br>
             <input id="nom" class="form-control" name="nom" type="text" placeholder="Surname..." value="<?php echo isset($_POST["nom"]) ? esc_attr($_POST["nom"]) : ''; ?>">
         </div>
         <div class="form-group">
-            <label for="diplome">Document Number</label><br>
+            <label for="diplome"><?php echo $labels['documentNumber']; ?></label><br>
             <input id="diplome" class="form-control" name="diplome" type="text" placeholder="Number..." value="<?php echo isset($_POST["diplome"]) ? esc_attr($_POST["diplome"]) : ''; ?>">
         </div>
         <p>
-            <input type="submit" name="SubmitButton"  class="btn btn-primary"  value="Validate">
+            <input type="submit" name="SubmitButton"  class="btn btn-primary"  value="<?php echo $labels['validate']; ?>">
         </p>
     </form>
 </div>
--- epfl-diploma-verification.php
+++ epfl-diploma-verification.php
@@ -7,9 +7,42 @@ Author:       Rosa Maggi
 License: Copyright (c) 2021 Ecole Polytechnique Federale de Lausanne, Switzerland
 */
 
-function epfl_diploma_verification_process_shortcode()
-{  
+function epfl_diploma_verification_process_shortcode($atts)
+{
     ob_start();
+
+    $atts = shortcode_atts([
+        'language' => ''
+    ], $atts);
+    if ($atts['language'] == 'FR') {
+        $language = 'fr';
+        $labels = [
+            'graduateName' => 'Nom du diplômé',
+            'graduateFirstName' => 'Prénom du diplômé',
+            'graduateSurname' => 'Nom du diplômé',
+            'documentNumber' => 'Numéro du diplôme',
+            'validate' => 'Valider',
+            'documentTitle' => 'Titre du document',
+            'error' => 'Impossible de vérifier le document',
+            'errorMessage' => 'Merci de contacter le <a href="https://studying.epfl.ch/student_desk">Guichet des étudiants</a>',
+            'authenticationConfirmed' => 'Authenticité confirmée'
+        ];
+
+    } else {
+        $language = 'en';
+        $labels = [
+            'graduateName' => 'Graduate’s Name',
+            'graduateFirstName' => 'Graduate’s First Name',
+            'graduateSurname' => 'Graduate’s Surname',
+            'documentNumber' => 'Document Number',
+            'validate' => 'Validate',
+            'documentTitle' => 'Document Title',
+            'error' => 'Unable to verify document',
+            'errorMessage' => 'Please contact the <a href="https://studying.epfl.ch/student_desk">Student Services Desk</a>',
+            'authenticationConfirmed' => 'Authenticity confirmed'
+        ];
+    }
+
     wp_enqueue_style( 'epfl_diploma_verification_style', plugin_dir_url(__FILE__).'css/styles.css', [], '2.1');
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -39,7 +72,7 @@ function epfl_diploma_verification_process_shortcode()
         }
 
         include('diploma_verification_form.php');
-        call_web_service($prenom, $nom, $diplome);
+        call_web_service($prenom, $nom, $diplome, $labels);
         return ob_get_clean();
     } else {
         include('diploma_verification_form.php');
@@ -49,7 +82,7 @@ function epfl_diploma_verification_process_shortcode()
     }
 }
 
-function call_web_service($prenom, $nom, $diplome){
+function call_web_service($prenom, $nom, $diplome, $labels){
     $apiUrl = "https://isa.epfl.ch/services/diplome/" . $diplome . "/validate";
     $formData = array(
         'prenom' => $prenom,
@@ -69,32 +102,32 @@ function call_web_service($prenom, $nom, $diplome){
 
     if ($httpCode === 200) {
         $data = json_decode($response, true);
-        success($data);
+        success($data, $labels);
     }else{
-        failure();
+        failure($labels);
     }
     curl_close($curl);
 }
 
-function success($data){
+function success($data, $labels){
     ?>
     <div id="success" class="row">
         <div class="status-success col-lg-12">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3>Authenticity confirmed</h3>
+                    <h3><label><?php echo $labels['authenticationConfirmed']; ?></label></h3>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-5"><label>Graduate’s Name</label></div>
+                <div class="col-lg-5"><label><?php echo $labels['graduateName']; ?></label></div>
                 <div id="r-nom" class="col-lg-7"><?php echo $data[0]['fullName'] ?></div>
             </div>
             <div class="row">
-                <div class="col-lg-5"><label>Document Number</label></div>
+                <div class="col-lg-5"><label><?php echo $labels['documentNumber']; ?></label></div>
                 <div id="r-diplome" class="col-lg-7"><?php echo $data[0]['numero'] ?></div>
             </div>
             <div class="row">
-                <div class="col-lg-5"><label>Document Title</label></div>
+                <div class="col-lg-5"><label><?php echo $labels['documentTitle']; ?></label></div>
                 <div id="r-titre" class="col-lg-7"><?php echo $data[0]['title'] ?></div>
             </div>
         </div>
@@ -102,13 +135,13 @@ function success($data){
     <?php
 }
 
-function failure(){
+function failure($labels){
     ?>
     <div id="failure" class="row">
         <div class="status-failure col-lg-12">
-            <h3>Unable to verify document</h3>
+            <h3><?php echo $labels['error']; ?></h3>
             <div class="row">
-                <div class="col-lg-12">Please contact the <a href="https://studying.epfl.ch/student_desk">Student Services Desk</a></div>
+                <div class="col-lg-12"><?php echo $labels['errorMessage']; ?></div>
             </div>
         </div>
     </div>
